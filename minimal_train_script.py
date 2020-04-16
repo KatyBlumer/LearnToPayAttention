9b425b05f213fd605fc1e696da6f62270e1a552e
Katy Blumer <katherine.blumer@gmail.com>
2020-04-16 04:11:52 -0400

    [param] batch size 32 (from 16), epochs 10

--- minimal_train_script.py
+++ minimal_train_script.py
@@ -29,6 +29,8 @@ parser.add_argument("--RUN_MULTI", action='store_true', help='if True, rerun thi
 
 parser.add_argument("--BATCH_SIZE", type=int, default=16, help="batch size")
 parser.add_argument("--EPOCHS", type=int, default=20, help="number of epochs")
+parser.add_argument("--BATCH_SIZE", type=int, default=32, help="batch size")
+parser.add_argument("--EPOCHS", type=int, default=10, help="number of epochs")
 parser.add_argument("--LR", type=float, default=0.1, help="initial learning rate")
 parser.add_argument("--LOG_DIR", type=str, default="logs", help='path of log files')
 
